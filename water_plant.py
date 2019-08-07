a4d755e991caa216a8c877c0d816754a45034184
Pedro Mariano <plmariano@fc.ul.pt>
2019-08-07 14:38:08 +0100

    Fixed typos and indentation.

--- water_plant.py
+++ water_plant.py
@@ -18,7 +18,7 @@ import masterflex
 DATA_FOLDER = '/home/pi/water-weight-control'
 PLANT_WEIGHT_FILENAME = DATA_FOLDER + '/peso-plantas.csv'
 CONFIG_FILENAME = DATA_FOLDER + '/config.txt'
-WATERINGS_FILENAME = DATA_FOLDER + '/watering.csv'
+WATERING_FILENAME = DATA_FOLDER + '/watering.csv'
 
 MOTOR_SPEED = 70
 
@@ -32,7 +32,7 @@ WATER_PER_1_REVOLUTION = 0.85
 def main ():
     play_sound ('welcome-message.riff')
     cfg = read_config ()
-    upload_waterings (cfg ['token'])
+    upload_watering (cfg ['token'])
     barcode = detect_barcode_scanner ()
     pump = detect_pump ()
     try:
@@ -59,14 +59,14 @@ def read_config ():
 
 
 def detect_barcode_scanner ():
-    """Wait for the barcode scanner to be connected to the raspberrypi.
+    """Wait for the barcode scanner to be connected to the raspberry pi.
 
     Per the `dmesg` command, the barcode scanner is assigned the device filename
 
         /dev/input/by-id/usb-SHANG_CHEN_DIAN_ZI_SHANG_CHEN_HID_SC-32-event-kbd
 
     We don't check for this file, instead we check for '/dev/hidraw0' which
-    is the only input device that will be connected to the raspberrypi.
+    is the only input device that will be connected to the raspberry pi.
     """
     device = '/dev/hidraw0'
     while not os.path.exists (device):
@@ -136,15 +136,25 @@ def read_plant_weight_file ():
             delimiter=',',
         )
         result = {
-            int (row ['id']) : row ['weight']
+            int (row ['id']): row ['weight']
             for row in reader
             }
     return result
 
 
-hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm', 17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';' , 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'  }
+hid = {
+    4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm',
+    17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z',
+    30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=',
+    47: '[', 48: ']', 49: '\\', 51: ';', 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'
+}
 
-hid2 = { 4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G', 11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M', 17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23: 'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y', 29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ', 45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':' , 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'  }
+hid2 = {
+    4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G', 11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M',
+    17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23: 'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y', 29: 'Z',
+    30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ', 45: '_', 46: '+',
+    47: '{', 48: '}', 49: '|', 51: ':', 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'
+}
 
 
 def get_plant_code_reading (barcode_scanner_device):
@@ -160,34 +170,34 @@ def get_plant_code_reading (barcode_scanner_device):
     shift = False
     done = False
     while not done:
-    # Get the character from the HID
+        # Get the character from the HID
         buffer = fd.read (8)
         for c in buffer:
-          if ord (c) > 0:
-             ##  40 is carriage return which signifies
-             ##  we are done looking for characters
-             if int (ord (c)) == 40:
-                done = True
-                break;
-             ##  If we are shifted then we have to
-             ##  use the hid2 characters.
-             if shift:
-                ## If it is a '2' then it is the shift key
-                if int (ord (c)) == 2 :
-                   shift = True
-                ## if not a 2 then lookup the mapping
+            if ord (c) > 0:
+                # 40 is carriage return which signifies
+                # we are done looking for characters
+                if int (ord (c)) == 40:
+                    done = True
+                    break
+                # If we are shifted then we have to
+                # use the hid2 characters.
+                if shift:
+                    # If it is a '2' then it is the shift key
+                    if int (ord (c)) == 2:
+                        shift = True
+                    # if not a 2 then lookup the mapping
+                    else:
+                        ss += hid2[int (ord (c))]
+                        shift = False
+                # If we are not shifted then use
+                # the hid characters
                 else:
-                   ss += hid2 [int (ord (c)) ]
-                   shift = False
-             ##  If we are not shifted then use
-             ##  the hid characters
-             else:
-                ## If it is a '2' then it is the shift key
-                if int (ord (c)) == 2 :
-                   shift = True
-                ## if not a 2 then lookup the mapping
-                else:
-                   ss += hid [int (ord (c))]
+                    # If it is a '2' then it is the shift key
+                    if int (ord (c)) == 2:
+                        shift = True
+                    # if not a 2 then lookup the mapping
+                    else:
+                        ss += hid[int (ord (c))]
     fd.close ()
     result = int (ss)
     write_to_log ('read plant code {}'.format (result))
@@ -238,7 +248,7 @@ def record_watering (plant_id, plant_current_weight, plant_desired_weight, motor
     Record a watering event.
     """
     now = datetime.datetime.now ()
-    with open (WATERINGS_FILENAME, 'at') as fd:
+    with open (WATERING_FILENAME, 'at') as fd:
         fd.write ('"{}",{},{},{},{},{},{}\n'.format (
             now.isoformat (),
             plant_id,
@@ -251,19 +261,19 @@ def record_watering (plant_id, plant_current_weight, plant_desired_weight, motor
         )
 
 
-def upload_waterings (token):
+def upload_watering (token):
     try:
         dbx = dropbox.Dropbox (token)
         write_to_log ('connected to dropbox account')
         dbx.files_upload (
-            WATERINGS_FILENAME,
-            '/waterings.csv',
+            WATERING_FILENAME,
+            '/watering.csv',
             mode=dropbox.files.WriteMode.overwrite,
         )
-        write_to_log ('uploaded waterings file')
+        write_to_log ('uploaded watering file')
         result = True
     except BaseException as ex:
-        write_to_log ('an error occur while uploading waterings file {}'.format (ex))
+        write_to_log ('an error occur while uploading watering file {}'.format (ex))
         result = False
     return result
 
@@ -316,7 +326,7 @@ def wait_for_file (filename, timeout, check_time = 1):
 
     This function is used by the application to wait for devices to be
     connected to the raspberry pi.  The function waits at most `timeout`
-    seconds and checks the existance of the file every `check_time`
+    seconds and checks the existence of the file every `check_time`
     seconds.
 
     :param filename: the device filename to wait for.
@@ -325,10 +335,10 @@ def wait_for_file (filename, timeout, check_time = 1):
 
     :return: `True` if the device filename exists.
     """
-    ellapsed = 0
-    while not os.path.exists (filename) and ellapsed < timeout:
+    elapsed = 0
+    while not os.path.exists (filename) and elapsed < timeout:
         time.sleep (check_time)
-        ellapsed += check_time
+        elapsed += check_time
     return os.path.exists (filename)
 
 
